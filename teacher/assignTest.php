048e72ca2f538f07fee36e419cc5d158ec070db0
parchemaster <86682020+parchemaster@users.noreply.github.com>
2023-05-18 22:12:36 +0200

    nav bar assignTest

--- teacher/assignTest.php
+++ teacher/assignTest.php
@@ -56,7 +56,14 @@ try {
 
 <head>
     <title>teacher</title>
-    
+            <!-- Bootstrap -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
     <script src="assignTest.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
