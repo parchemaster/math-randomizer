451344101be773a72fe6d8bb8502dee6d1c4c1bd
Riuminia <xriumin@stuba.sk>
2023-05-19 19:23:23 +0200

    last2

--- script/result.js
+++ script/result.js
@@ -1,25 +1,15 @@
 
 
-<<<<<<< HEAD
 
 function checkResult(sectionId, id) {
-=======
-function checkResult(sectionId,id) {
-    console.log(mathDiv);
-    console.log(sectionId);
->>>>>>> c25aaafbe7b1af0f287629b068bf5c532fea254e
     $.ajax({
         type: 'POST',
         url: "../server/mathResult.php",
         data: {
             number: randomNumber,
             result: parseLatex(mathDiv.textContent),
-<<<<<<< HEAD
             id: id,
             rawResult:mathDiv.textContent
-=======
-            id:id
->>>>>>> c25aaafbe7b1af0f287629b068bf5c532fea254e
         },
         success: function (msg) {
             //rightAnswer=parseLatex(msg);
--- teacher/createQuestion.php
+++ teacher/createQuestion.php
@@ -24,17 +24,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Create a question</title>
-<<<<<<< HEAD
-=======
-            <!-- Bootstrap -->
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-
->>>>>>> c25aaafbe7b1af0f287629b068bf5c532fea254e
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -58,16 +47,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 </head>
 
 <body>
-<<<<<<< HEAD
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-    <a class="navbar-brand" href="student_index.php">Hello, <?php echo $_SESSION["fullname"] ?></a>
-=======
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="#">Hello, <?php echo $_SESSION["fullname"] ?></a>
->>>>>>> c25aaafbe7b1af0f287629b068bf5c532fea254e
+    <a class="navbar-brand" href="teacher_index.php">Hello, <?php echo $_SESSION["fullname"] ?></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -91,16 +73,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                 <li class="nav-item">
                     <a class="nav-link" href="../auth/logout.php">Logout</a>
                 </li>
-<<<<<<< HEAD
-                </li>
-                <li class="nav-item">
-                <select name="language" id="languageSwitcher"></select>
-            </li>
-=======
                 <li class="nav-item">
                     <select name="language" id="languageSwitcher"></select>
                 </li>
->>>>>>> c25aaafbe7b1af0f287629b068bf5c532fea254e
             </ul>
       </div>
     </div>
